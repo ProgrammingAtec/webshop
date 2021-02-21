9233edb43d9590b311052bcbf304d3f3f177468f
chingiz egamberdiev <chingizegamberdiev@chingizs-iMac.local>
2021-02-21 19:22:08 +0300

    frontend initial commit

---
+++ WebShop/frontend/src/app/app-routing.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { AboutComponent } from "./about/about.component";
+import { DeliveryComponent } from "./delivery/delivery.component";
+import { HomeComponent } from "./home/home.component";
+
+const routes: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'delivery', component: DeliveryComponent },
+    { path: '**', component: AboutComponent }
+]
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ WebShop/frontend/src/app/delivery/delivery.module.ts
@@ -0,0 +1,8 @@
+import { NgModule } from "@angular/core";
+import { DeliveryComponent } from "./delivery.component";
+
+@NgModule({
+    declarations: [DeliveryComponent],
+    exports: [DeliveryComponent]
+})
+export class DeliveryModule {}
