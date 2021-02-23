fa7068e364843f2b7d387a0008b4995d1e326caa
Alexander Duvanov <alexander.duvanov@testit.ru>
2021-02-23 21:23:53 +0300

    Create new solution with api

--- WebShop/WebShop.Api/obj/Debug/netcoreapp3.1/WebShop.Api.AssemblyInfo.cs
+++ WebShop/WebShop.Api/obj/Debug/netcoreapp3.1/WebShop.Api.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("WebShop")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("WebShop.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("WebShop")]
-[assembly: System.Reflection.AssemblyTitleAttribute("WebShop")]
+[assembly: System.Reflection.AssemblyProductAttribute("WebShop.Api")]
+[assembly: System.Reflection.AssemblyTitleAttribute("WebShop.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
--- WebShop/frontend/src/app/delivery/delivery.module.ts
+++
@@ -1,8 +0,0 @@
-import { NgModule } from "@angular/core";
-import { DeliveryComponent } from "./delivery.component";
-
-@NgModule({
-    declarations: [DeliveryComponent],
-    exports: [DeliveryComponent]
-})
-export class DeliveryModule {}
