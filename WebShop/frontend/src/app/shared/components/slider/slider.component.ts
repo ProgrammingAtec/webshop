1b9548a7d47790d4b13793874bab6773144b3216
chingiz egamberdiev <chingizegamberdiev@chingizs-iMac.local>
2021-05-20 21:22:23 +0300

    mock data, docker

--- WebShop/frontend/src/app/shared/components/slider/slider.component.ts
+++ WebShop/frontend/src/app/shared/components/slider/slider.component.ts
@@ -1,51 +1,13 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { LayoutService } from '../../services/layout.service';
+import { items } from './slider.mock';
 
-interface ProductModel {
+export interface ProductModel {
   imageUrl: string;
   name: string;
   previewDescription: string;
   price: string;
 }
-
-const items: ProductModel[] = [
-  {
-    imageUrl: 'casual_gucci.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '1 990$'
-  },
-  {
-    imageUrl: 'casual_gucci2.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '3 990$'
-  },
-  {
-    imageUrl: 'casual_gucci3.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '1 490$'
-  },
-  {
-    imageUrl: 'casual_gucci.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '1 990$'
-  },
-  {
-    imageUrl: 'casual_gucci2.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '3 990$'
-  },
-  {
-    imageUrl: 'casual_gucci3.png',
-    name: 'Brand Textile Yellow Skirt',
-    previewDescription: 'Preside Description',
-    price: '1 490$'
-  },
-]
 
 @Component({
   selector: 'app-slider',
@@ -69,6 +31,6 @@ export class SliderComponent {
     const sliderScrolled = this.slider.nativeElement.scrollLeft;
     const scrolledItemsNumber = Math.round(direction === 'right' ? sliderScrolled / sliderItemLength + 1 : sliderScrolled / sliderItemLength - 1);
 
-    sliderItems[scrolledItemsNumber]?.scrollIntoView({ behavior: 'smooth', inline: 'start' }); // optional chaining for initial position
+    sliderItems[scrolledItemsNumber]?.scrollIntoView({ behavior: 'smooth', inline: 'start', block: 'nearest' }); // optional chaining for initial position
   }
 }
